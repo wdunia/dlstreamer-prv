6f45df1b7d8d5590410e6a9fdfaa7d317fdbfdf1
pbartosik <pawelbrtsk@gmail.com>
2026-03-30 13:29:56 +0200

    ut fix (#730)

--- tests/unit_tests/tests_gstgva/test_pipeline_optimizer.py
+++ tests/unit_tests/tests_gstgva/test_pipeline_optimizer.py
@@ -5,7 +5,7 @@
 # ==============================================================================
 
 import unittest
-from optimizer import get_optimized_pipeline # pylint: disable=no-name-in-module
+from optimizer import DLSOptimizer # pylint: disable=no-name-in-module
 from utils import get_model_path
 
 class TestOptimizer(unittest.TestCase):
@@ -13,7 +13,9 @@ class TestOptimizer(unittest.TestCase):
     def test_optimizer_works(self):
         model_path = get_model_path("yolo11s")
         pipeline = f"urisourcebin buffer-size=4096 uri=https://videos.pexels.com/video-files/1192116/1192116-sd_640_360_30fps.mp4 ! decodebin ! gvadetect model={model_path} ! queue ! gvawatermark ! vah264enc ! h264parse ! mp4mux ! fakesink"
-        optimized_pipeline, fps = get_optimized_pipeline(pipeline, search_duration=100)
+        optimizer = DLSOptimizer()
+        optimizer.set_search_duration(100)
+        optimized_pipeline, fps = optimizer.optimize_for_fps(pipeline)
     
         self.assertIsNotNone(optimized_pipeline, "Optimizer did not return optimized pipeline")
         self.assertIsNotNone(fps, "Optimizer did not return FPS value")
